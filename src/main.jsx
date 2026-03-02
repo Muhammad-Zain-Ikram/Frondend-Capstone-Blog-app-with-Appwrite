04500aa220609c2dd3d922107402c13d3de445c0
Zain IKram <143309395+Muhammad-Zain-Ikram@users.noreply.github.com>
2026-03-02 12:38:37 +0000

    Correct routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import {AuthLayut, Layout} from "./components/index.js"
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
-import {AddPost, AllPost, EditPost, LandingPage,LoginPage, SignUpPage} from './pages/index.js'
+import {AddPost, AllPost, EditPost, LandingPage,LoginPage, Post, SignUpPage} from './pages/index.js'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
@@ -16,23 +16,33 @@ const router = createBrowserRouter(
         <AuthLayut authentication={false}>
             <LoginPage/>
       </AuthLayut>}/>
+
       <Route path='/sign-up' element={
         <AuthLayut authentication={false}>
             <SignUpPage/>
       </AuthLayut>}/>
+
       <Route path='/all-post' element={
         <AuthLayut authentication>
           <AllPost/>
         </AuthLayut>
       }/>
+
       <Route path='/add-post' element={
         <AuthLayut authentication>
           <AddPost/>
         </AuthLayut>
       }/>
+
       <Route path='/edit-post' element={
         <AuthLayut authentication>
           <EditPost/>
+        </AuthLayut>
+      }/>
+      
+      <Route path='/blog/:slug' element={
+        <AuthLayut authentication>
+          <Post/>
         </AuthLayut>
       }/>
     </Route>
@@ -40,7 +50,9 @@ const router = createBrowserRouter(
 )
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
   <Provider store={store}>
     <RouterProvider  router={router}/>
   </Provider>
+  </StrictMode>
 )
